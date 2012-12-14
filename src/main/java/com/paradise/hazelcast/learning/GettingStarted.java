4c119c76a0a07cc974a9c3b821ecf590db9b7eab
James Liu <james.liu@autodesk.com>
2012-12-14 11:25:00 +0800

    Some samples

---
+++ src/main/java/com/paradise/hazelcast/learning/GettingStarted.java
@@ -0,0 +1,26 @@
+package com.paradise.hazelcast.learning;
+
+import java.util.Map;
+import java.util.Queue;
+
+import com.hazelcast.core.Hazelcast;
+
+public class GettingStarted {
+    public static void main(String[] args) {
+        Map<Integer, String> mapCustomers = Hazelcast.getMap("customers");
+        mapCustomers.put(1, "Joe");
+        mapCustomers.put(2, "Ali");
+        mapCustomers.put(3, "Avi");
+
+        System.out.println("Customer with key 1: " + mapCustomers.get(1));
+        System.out.println("Map Size:" + mapCustomers.size());
+
+        Queue<String> queueCustomers = Hazelcast.getQueue("customers");
+        queueCustomers.offer("Tom");
+        queueCustomers.offer("Mary");
+        queueCustomers.offer("Jane");
+        System.out.println("First customer: " + queueCustomers.poll());
+        System.out.println("Second customer: " + queueCustomers.peek());
+        System.out.println("Queue size: " + queueCustomers.size());
+    }
+}
